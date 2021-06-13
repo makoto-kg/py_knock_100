1a04bb6c5fc51d4187fa14f200b5ce13120a55fe
makoto-kg <neapoli.smith@gmail.com>
2021-06-13 11:11:34 +0900

    Add 62, 63.

--- work13.py
+++ work13.py
@@ -1,8 +1,12 @@
 from knocks import knock061
+from knocks import knock062
+from knocks import knock063
 
 
 def exec():
     knock061.exec()
+    knock062.exec()
+    knock063.exec()
 
 
 if __name__ == "__main__":
