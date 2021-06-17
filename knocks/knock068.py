6c15d0132f619cb357924eea00a1c436bf1d5382
Makoto Kogoh <neapoli.smith@gmail.com>
2021-06-18 08:30:09 +0900

    Add 68.

---
+++ knocks/knock068.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def exec():
+    D = np.random.uniform(0, 1, 100)
+    S = np.random.randint(0, 10, 100)
+    D_sums = np.bincount(S, weights=D)
+    D_counts = np.bincount(S)
+    D_means = D_sums / D_counts
+    print(D_means)
--- work14.py
+++ work14.py
@@ -1,10 +1,12 @@
 from knocks import knock066
 from knocks import knock067
+from knocks import knock068
 
 
 def exec():
     knock066.exec()
     knock067.exec()
+    knock068.exec()
 
 
 if __name__ == "__main__":
