99a841a2332a9c1b50482101d7eb1d84c37fbc8c
Makoto Kogoh <neapoli.smith@gmail.com>
2021-06-23 14:41:24 +0900

    Add 73.

--- work15.py
+++ work15.py
@@ -1,10 +1,12 @@
 from knocks import knock071
 from knocks import knock072
+from knocks import knock073
 
 
 def exec():
     knock071.exec()
     knock072.exec()
+    knock073.exec()
 
 
 if __name__ == '__main__':
