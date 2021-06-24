dd2658067fb3736bc2be38d8c80702cfb6db613f
Makoto Kogoh <neapoli.smith@gmail.com>
2021-06-24 10:54:02 +0900

    add 74.

---
+++ knocks/knock074.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def exec():
+    C = np.bincount([1,1,2,3,4,4,6])
+    A = np.repeat(np.arange(len(C)), C)
+    print(A)
--- work15.py
+++ work15.py
@@ -1,12 +1,14 @@
 from knocks import knock071
 from knocks import knock072
 from knocks import knock073
+from knocks import knock074
 
 
 def exec():
     knock071.exec()
     knock072.exec()
     knock073.exec()
+    knock074.exec()
 
 
 if __name__ == '__main__':
