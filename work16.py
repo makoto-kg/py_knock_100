992066ad6ff7a17c15dbe8c84a51f4aef40b8ffe
makoto-kg <neapoli.smith@gmail.com>
2021-06-28 15:19:13 +0900

    add 78

--- work16.py
+++ work16.py
@@ -1,10 +1,12 @@
 from knocks import knock076
 from knocks import knock077
+from knocks import knock078
 
 
 def exec():
     knock076.exec()
     knock077.exec()
+    knock078.exec()
 
 
 if __name__ == "__main__":
