b149f340f811c8721bfcaf43f06cc1c561936a8a
Makoto Kogoh <makotox@jp.ibm.com>
2021-06-30 14:34:07 +0900

    Add 80.

--- work16.py
+++ work16.py
@@ -2,6 +2,7 @@ from knocks import knock076
 from knocks import knock077
 from knocks import knock078
 from knocks import knock079
+from knocks import knock080
 
 
 def exec():
@@ -9,6 +10,7 @@ def exec():
     knock077.exec()
     knock078.exec()
     knock079.exec()
+    knock080.exec()
 
 
 if __name__ == "__main__":
