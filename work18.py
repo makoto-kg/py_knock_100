40bf4123c7c67f5f3342f01c61d1d02d4801118e
Makoto Kogoh <makotox@jp.ibm.com>
2021-07-09 19:06:09 +0900

    Add 88

--- work18.py
+++ work18.py
@@ -1,10 +1,12 @@
 from knocks import knock086
 from knocks import knock087
+from knocks import knock088
 
 
 def exec():
     knock086.exec()
     knock087.exec()
+    knock088.exec()
 
 
 if __name__ == "__main__":
