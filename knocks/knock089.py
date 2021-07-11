1106d36db1ba5f6539df2703dc1f90f0ca35b857
Makoto Kogoh <neapoli.smith@gmail.com>
2021-07-11 09:16:55 +0900

    add 89, 90

---
+++ knocks/knock089.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def exec():
+    Z = np.arange(10000)
+    np.random.shuffle(Z)
+    n = 5
+
+    print(Z[np.argsort(Z)[-n:]])
--- work18.py
+++ work18.py
@@ -1,12 +1,16 @@
 from knocks import knock086
 from knocks import knock087
 from knocks import knock088
+from knocks import knock089
+from knocks import knock090
 
 
 def exec():
     knock086.exec()
     knock087.exec()
     knock088.exec()
+    knock089.exec()
+    knock090.exec()
 
 
 if __name__ == "__main__":
