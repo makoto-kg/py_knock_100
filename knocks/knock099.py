d9699e6ca2bb195ac0f02156a8dc53c39b585d14
Makoto Kogoh <neapoli.smith@gmail.com>
2021-07-21 00:04:15 +0900

    add 99

---
+++ knocks/knock099.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def exec():
+    X = np.asarray([[1.0, 0.0, 3.0, 8.0],
+                    [2.0, 0.0, 1.0, 1.0],
+                    [1.5, 2.5, 1.0, 0.0]])
+    n = 4
+    M = np.logical_and.reduce(np.mod(X, 1) == 0, axis=-1)
+    M &= (X.sum(axis=-1) == n)
+    print(X[M])
--- work20.py
+++ work20.py
@@ -1,12 +1,14 @@
 from knocks import knock096
 from knocks import knock097
 from knocks import knock098
+from knocks import knock099
 
 
 def exec():
     knock096.exec()
     knock097.exec()
     knock098.exec()
+    knock099.exec()
 
 
 if __name__ == "__main__":
